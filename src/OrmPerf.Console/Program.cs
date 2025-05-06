e37cfd5cdd7cd8e96d8084d8af414172cfde9c26
SentiWW <sentiww@gmail.com>
2025-05-06 23:10:18 +0200

    Removed unused code

--- src/OrmPerf.Console/Program.cs
+++ src/OrmPerf.Console/Program.cs
@@ -3,8 +3,6 @@ using System.Reflection;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
-Environment.GetCommandLineArgs();
-
 var scenariosAssembly = Assembly.GetExecutingAssembly();
 
 var config = Debugger.IsAttached ? new DebugInProcessConfig() : null;
